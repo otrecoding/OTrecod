8ae75f9ea88d7dbd369e7ede051ae2e7855d6d7f
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-06-04 22:33:28 +0200

    Update to version 406

--- R/tab_test.r
+++ R/tab_test.r
@@ -5,8 +5,8 @@
 #' @format A data frame with 5000 rows and 6 variables:
 #' \describe{
 #'   \item{ident}{identificator, 1 or 2}
-#'   \item{Y1}{covariable 1, observed for ident=1 and unobserved for ident=2}
-#'   \item{Y2}{covariable 2, observed for ident=2 and unobserved for ident=1}
+#'   \item{Y1}{outcome 1, observed for ident=1 and unobserved for ident=2}
+#'   \item{Y2}{outcome 2, observed for ident=2 and unobserved for ident=1}
 #'   \item{X1}{covariable 1, integer}
 #'   \item{X2}{covariable 2, integer}
 #'   \item{X3}{covariable 3, integer}
