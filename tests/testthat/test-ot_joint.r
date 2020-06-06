c3fef088014d256ddbede632bb9ce5441bda0572
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-06-06 22:56:38 +0200

    Update test-ot_joint.r

--- tests/testthat/test-ot_joint.r
+++ tests/testthat/test-ot_joint.r
@@ -1,10 +1,18 @@
 test_that("ot_joint works", {
 
   data(tab_test)
-  tab_test2 = tab_test[c(1:75,5001:5075),1:5]
 
-  try1J = OT_joint(tab_test2, nominal = c(1,4:5), ordinal = c(2,3),
+  n <- 75
+  tab_test2 = tab_test[c(1:n,5001:(5000+n)),1:5]
+
+  try = OT_joint(tab_test2, nominal = c(1,4:5), ordinal = c(2,3),
                    dist.choice = "M", which.DB = "B")
 
-  expect_equal(2 * 2, 4)
+  print(try$GAMMA_A)
+  ref = matrix(data=c(1.4133405, 0.4799928, 0.0000000,
+                      0.0000000, 0.0000000, 0.6266879,
+                      0.5199788, 0.0000000, 0.0000000,
+                      0.0000000, 0.4758268, 1.0175065), nrow=4)
+
+  expect_equal(all(mapply(all.equal, try$gamma_B, ref, tolerance=1e-6)), TRUE)
 })
