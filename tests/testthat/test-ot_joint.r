63d7aef32b0322edf8577adc55590f4dd022d858
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-06-07 11:08:48 +0200

    Update test-ot_joint.r

--- tests/testthat/test-ot_joint.r
+++ tests/testthat/test-ot_joint.r
@@ -2,17 +2,20 @@ test_that("ot_joint works", {
 
   data(tab_test)
 
-  n <- 75
-  tab_test2 = tab_test[c(1:n,5001:(5000+n)),1:5]
+  tab_test2 = tab_test[c(1:5000,5001:10000),1:5]
 
   try = OT_joint(tab_test2, nominal = c(1,4:5), ordinal = c(2,3),
-                   dist.choice = "M", which.DB = "B")
+                   dist.choice = "M", which.DB = "BOTH")
 
   print(try$GAMMA_A)
-  ref = matrix(data=c(1.4133405, 0.4799928, 0.0000000,
-                      0.0000000, 0.0000000, 0.6266879,
-                      0.5199788, 0.0000000, 0.0000000,
-                      0.0000000, 0.4758268, 1.0175065), nrow=4)
+  ref_B = matrix(data=c( 1.309845, 0.4341554, 0.0000000, 0.000000,
+                          0.000000, 0.7382913, 0.7677087, 0.000000,
+                          0.000000, 0.0000000, 0.3163772, 1.026423), nrow=4)
 
-  expect_equal(all(mapply(all.equal, try$gamma_B, ref, tolerance=1e-6)), TRUE)
+  ref_A = matrix(data=c(1.3216, 0.4380475, 0.0000000, 0.0000,
+                         0.0000, 0.7443525, 0.7737806, 0.0000,
+                         0.0000, 0.0000000, 0.3186194, 1.0332), nrow=4)
+
+  expect_equal(all(mapply(all.equal, try$gamma_A, ref_A, tolerance=1e-6)), TRUE)
+  expect_equal(all(mapply(all.equal, try$gamma_B, ref_B, tolerance=1e-6)), TRUE)
 })
